64f8e873926101902e021367f388b21822473aef
dexter <handexter000@gmail.com>
2022-11-26 15:15:07 -0500

    Finished step 40

---
+++ JL-MeanyBird1/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    //Game Over Canvas that is used for the game
+    [Header("Game Over UI Object for displaying Game Over Screen")]
+    public GameObject gameOverCanvas;
+    //Score Canvas that is used for the game
+    [Header("Score UI Object for display Score")]
+    public GameObject scoreCanvas;
+    //Spawner object that is used for the game
+    [Header("Spawner Onject for the spawning objects in game")]
+    public GameObject spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        scoreCanvas.SetActive(true);
+        gameOverCanvas.SetActive(false);
+        spawner.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        spawner.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+}

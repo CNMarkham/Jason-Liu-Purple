24f65f6ae3990d5e73042d6d4cd012891a6e58d9
Jason <tty76077@gmail.com>
2023-02-05 13:59:29 -0500

    Start step 32

--- Dropping Bombs2-JL/Assets/Spawner.cs
+++ Dropping Bombs2-JL/Assets/Spawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
     public GameObject bombPrefab;
     public float delay = 2.0f;
     public bool active = true;
@@ -12,24 +13,33 @@ public class Spawner : MonoBehaviour
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
+
+
     // Start is called before the first frame update
     void Start()
     {
         ResetDelay();
         StartCoroutine(EnemyGenerator());
 
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2;
+        objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
     }
 
     IEnumerator EnemyGenerator()
     {
         yield return new WaitForSeconds(delay);
-        if(active)
+        if (active)
         {
-            Instantiate(bombPrefab, new Vector3(randomX, SpawnY, 0), bombPrefab.transform.rotation);
+            float randomX = Random.Range(screenBounds.x - objectWidth, screenBounds.x * -1 + objectWidth);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
             ResetDelay();
         }
+
         StartCoroutine(EnemyGenerator());
     }
+
     void ResetDelay()
     {
         delay = Random.Range(delayRange.x, delayRange.y);

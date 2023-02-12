7a93b66db8782e0e27ab4ab9b65e713062aa0d30
Hayden <ngo.hayden10@gmail.com>
2023-02-12 14:00:38 -0500

    Add the four scripts

--- Dropping Bombs2-JL/Assets/Scripts/GameManager.cs
+++ Dropping Bombs2-JL/Assets/Scripts/GameManager.cs
@@ -1,28 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
+    public GameObject title;
+    private Vector3 screenbounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+    public GameObject splash;
+    public
 
-    private void Awake()
+    void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        Vector3 bob = new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z);
+        screenbounds = Camera.main.ScreenToWorldPoint(bob);
+        player = playerPrefab;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
+        splash.SetActive(false);
     }
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        splash.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+    }
+
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+        splash.SetActive(true);
+    }
+
+  
+
 
     // Update is called once per frame
     void Update()
     {
+        if (!gameStarted)
+        {
         if (Input.anyKeyDown)
         {
-            spawner.active = true;
+                ResetGame();
+        }
+        }else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenbounds.y) - 12 || !gameStarted)
+            {
+                Destroy(bombObject);
+            }
         }
     }
 }

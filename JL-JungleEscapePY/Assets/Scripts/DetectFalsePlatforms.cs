5cff034628703282bf691d365fe2a14f544fdc7d
Kelton <95721065+codeninjaskelton@users.noreply.github.com>
2023-04-28 19:56:10 -0400

    not done pv

---
+++ JL-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    private int PlatformLayer = LayerMask.GetMask("FalsePlatforms");
+    public bool raycast;
+
+    void Update()
+    {
+        
+        raycast = Physics.Raycast(transform.forward, Vector3.forward, .15f);
+
+
+    }
+}

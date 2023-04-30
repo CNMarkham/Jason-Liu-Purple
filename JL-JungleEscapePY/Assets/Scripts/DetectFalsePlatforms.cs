281f1b270f20f51af50c7a1f0df2630ea0517c5f
Bernice <bernice.huang101@gmail.com>
2023-04-30 12:51:45 -0400

    not done if statement

--- JL-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
+++ JL-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -9,9 +9,22 @@ public class DetectFalsePlatforms : MonoBehaviour
 
     void Update()
     {
-        
-        raycast = Physics.Raycast(transform.forward, Vector3.forward, .15f);
+        raycast = Physics.Raycast(transform.forward, Vector3.forward, .15f, 1 << 8);
+    }
 
+    void FixedUpdate()
+    {
+        int layerMask = 1 << 8;
 
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.Log("Did not Hit");
+        }
+        else
+        {
+            Debug.Log("Did not hit");
+        }
     }
 }
